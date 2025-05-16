2bcd5919257fff8ea85a4e5776b33dd2e7757d69
weibe_chen <a1236481@adelaide.edu.au>
2025-05-16 14:03:24 +0800

    Update instruction_memory.v
    reduce white space

--- src/instruction_memory.v
+++ src/instruction_memory.v
@@ -1,20 +1,15 @@
-`ifndef INSTRUCTION_MEMORY_V
-`define INSTRUCTION_MEMORY_V
 module instruction_memory(
 input clk,
 input [15:0] i_address,
 output reg [31:0] i_read_data
-
 );
 localparam RAM_DEPTH = 1 << 16;
 reg [31:0]memory[0:RAM_DEPTH];
 always @(posedge clk)begin
 i_read_data <= memory[i_address[15:2]]; // rm low bits（ 4 bytes in each instruction）
 end
-
 initial begin
-    $readmemh("program.hex", memory);  // c code compile hex file
+$readmemh("program.hex", memory);  // c code compile hex file
 end
-
 endmodule
-`endif
+
