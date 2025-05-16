997f055524f1daebe99d263c2b04cd86bf1f4d5a
weibe_chen <a1236481@adelaide.edu.au>
2025-05-16 13:59:44 +0800

    Update fetch.v
    reduce white space

--- src/fetch.v
+++ src/fetch.v
@@ -1,27 +1,17 @@
-`ifndef FETCH_V
-`define FETCH_V
 module fetch(
-  input clk,
-  input rst,
-  input next_pc_select,
-  input [15:0] pc_target,
-  output reg [15:0] pc
+input clk,
+input rst,
+input next_pc_select,
+input [15:0] pc_target,
+output reg [15:0] pc
 );
-
-
 always @(posedge clk) begin
-
-  if (rst)begin
-    pc <= 16'h0000;
-
-  end else if (next_pc_select) begin
-    
-    pc <= pc_target;
-     
-  end else begin
-    pc <= pc + 16'h4;
-    end
+if (rst)begin
+pc <= 16'h0000;
+end else if (next_pc_select) begin
+pc <= pc_target;
+end else begin
+pc <= pc + 16'h4;
 end
-
+end
 endmodule
-`endif
