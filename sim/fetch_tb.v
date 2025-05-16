850ed7c6addad5e5bcbd20292ac8a43d188f5c5b
weibe_chen <a1236481@adelaide.edu.au>
2025-05-16 14:15:17 +0800

    Update fetch_tb.v
    reduce white space

--- sim/fetch_tb.v
+++ sim/fetch_tb.v
@@ -1,4 +1,3 @@
-`include"fetch.v"
 module fetch_tb;
 reg  clk;
 reg  rst;
@@ -6,56 +5,50 @@ reg  next_pc_select;     //select jump
 wire [15:0]pc;
 reg [15:0]pc_target;	  //jump address
 fetch fetch(
-	.clk(clk),
-	.rst(rst),
-	.next_pc_select(next_pc_select),
-	.pc(pc),
-	.pc_target(pc_target)
+.clk(clk),
+.rst(rst),
+.next_pc_select(next_pc_select),
+.pc(pc),
+.pc_target(pc_target)
 );
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-  end
 initial begin
- clk = 1'b1;
-  rst = 1'b1;
-  next_pc_select = 1'b0;
-  pc_target = 16'h0000;
-
-  #1
-  #10
-  rst = 1'b0;
-
-  #10
-  $display("PC: %h", pc);
-
-  /***************************
-  * Add more test cases here *
-  ***************************/
-  #10 
-  pc_target= 16'h1010;
-  next_pc_select = 1'b0;
-  #10
-  $display("PC: %h", pc);
-  #10
-  pc_target =16'h1011;
-  next_pc_select = 1'b1;
-  #10
-  $display("PC: %h", pc);
-  #10
-  rst = 1'b1;
-  pc_target = 16'h1111;
-  next_pc_select = 1'b1;
-  #10
-  $display("PC: %h", pc);
-  #10
-  rst = 1'b0;
-  pc_target = 16'b0001;
-  next_pc_select = 1'b0;
-  #10
-  $display("PC: %h", pc);
-  #100
-  $finish;
+clk = 0;
+forever #5 clk = ~clk;
+end
+initial begin
+clk = 1'b1;
+rst = 1'b1;
+next_pc_select = 1'b0;
+pc_target = 16'h0000;
+#1
+#10
+rst = 1'b0;
+#10
+$display("PC: %h", pc);
+#10 
+pc_target= 16'h1010;
+next_pc_select = 1'b0;
+#10
+$display("PC: %h", pc);
+#10
+pc_target =16'h1011;
+next_pc_select = 1'b1;
+#10
+$display("PC: %h", pc);
+#10
+rst = 1'b1;
+pc_target = 16'h1111;
+next_pc_select = 1'b1;
+#10
+$display("PC: %h", pc);
+#10
+rst = 1'b0;
+pc_target = 16'b0001;
+next_pc_select = 1'b0;
+#10
+$display("PC: %h", pc);
+#100
+$finish;
 end
 
   
