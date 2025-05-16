214dafc0326b2970a3f173605bab1c28d74ffabc
weibe_chen <a1236481@adelaide.edu.au>
2025-05-16 14:10:36 +0800

    Update cpu_tb.v
    reduce white space

--- sim/cpu_tb.v
+++ sim/cpu_tb.v
@@ -1,26 +1,21 @@
 `timescale 1ns/1ps
-
 module top_tb;
-    reg clk;
-    reg rst;
-
-    // Instantiate your top module
-    top uut (
-        .clk(clk),
-        .rst(rst)
-         // 若有接 pc 輸出可觀察
-    );
-
-    // Clock generation
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // 100MHz clock
-    end
-
-    // Reset and simulation control
-    initial begin
-        rst = 1;
-        #10 rst = 0;
-        #1000 $finish;  // 模擬執行一段時間後停止
-    end
+reg clk;
+reg rst;
+// Instantiate your top module
+top uut (
+.clk(clk),
+.rst(rst)
+);
+// Clock generation
+initial begin
+clk = 0;
+forever #5 clk = ~clk; // 100MHz clock
+end
+// Reset and simulation control
+initial begin
+rst = 1;
+#10 rst = 0;
+#1000 $finish;  // 模擬執行一段時間後停止
+end
 endmodule
